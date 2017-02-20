8e5b757bc0202fdb6d2a337387bc8eac4d357652
jianjun.lin <jianjun.lin@baidao.com>
2017-02-20 11:00:11 +0800

    x

--- data/src/main/java/org/icegeneral/mvvm/data/AbstractDataProvider.java
+++ data/src/main/java/org/icegeneral/mvvm/data/AbstractDataProvider.java
@@ -1,10 +1,60 @@
 package org.icegeneral.mvvm.data;
 
 import org.icegeneral.mvvm.data.internal.webservice.WebService;
+import org.icegeneral.mvvm.data.internal.webservice.WebServiceProvider;
+import org.icegeneral.mvvm.data.model.News;
+import org.icegeneral.mvvm.data.model.TopNews;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.subscriptions.CompositeSubscription;
 
 /**
  * Created by jianjun.lin on 2017/2/17.
  */
 
 public abstract class AbstractDataProvider implements WebService {
+
+    protected Map<Object, AbstractDataProvider> providerMap = new WeakHashMap<>();
+
+    private CompositeSubscription subscription = new CompositeSubscription();
+
+    public void cancel(Object target) {
+        AbstractDataProvider provider = providerMap.get(target);
+        if (provider == null) {
+            return;
+        }
+        provider.cancel();
+    }
+
+    public void cancel() {
+        subscription.unsubscribe();
+        for (AbstractDataProvider provider : providerMap.values()) {
+            provider.cancel();
+        }
+    }
+
+    private <T> Observable.Operator<T, T> bind() {
+        return new Observable.Operator<T, T>() {
+            @Override
+            public Subscriber<? super T> call(Subscriber<? super T> subscriber) {
+                subscription.add(subscriber);
+                return subscriber;
+            }
+        };
+    }
+
+    @Override
+    public Observable<News> getNewsList(String date) {
+        return WebServiceProvider.get().getNewsList(date).lift(this.<News>bind());
+    }
+
+    @Override
+    public Observable<TopNews> getTopNewsList() {
+        return WebServiceProvider.get().getTopNewsList().lift(this.<TopNews>bind());
+    }
+
 }
--- data/src/main/java/org/icegeneral/mvvm/data/DataProvider.java
+++ data/src/main/java/org/icegeneral/mvvm/data/DataProvider.java
@@ -1,16 +1,5 @@
 package org.icegeneral.mvvm.data;
 
-import org.icegeneral.mvvm.data.internal.webservice.WebServiceProvider;
-import org.icegeneral.mvvm.data.model.News;
-import org.icegeneral.mvvm.data.model.TopNews;
-
-import java.util.Map;
-import java.util.WeakHashMap;
-
-import rx.Observable;
-import rx.Subscriber;
-import rx.subscriptions.CompositeSubscription;
-
 /**
  * Created by jianjun.lin on 2017/1/7.
  */
@@ -25,54 +14,16 @@ public class DataProvider extends AbstractDataProvider {
         return DataProviderHolder.INSTANCE;
     }
 
-    private Map<Object, DataProvider> providerMap = new WeakHashMap<>();
-    private CompositeSubscription subscription = new CompositeSubscription();
-
     private DataProvider() {
     }
 
     public AbstractDataProvider tag(Object target) {
-        DataProvider provider = providerMap.get(target);
+        AbstractDataProvider provider = providerMap.get(target);
         if (provider == null) {
             provider = new DataProvider();
             providerMap.put(target, provider);
         }
         return provider;
-    }
-
-    public void cancel(Object target) {
-        DataProvider provider = providerMap.get(target);
-        if (provider == null) {
-            return;
-        }
-        provider.cancel();
-    }
-
-    public void cancel() {
-        subscription.unsubscribe();
-        for (DataProvider provider : providerMap.values()) {
-            provider.cancel();
-        }
-    }
-
-    private <T> Observable.Operator<T, T> bind() {
-        return new Observable.Operator<T, T>() {
-            @Override
-            public Subscriber<? super T> call(Subscriber<? super T> subscriber) {
-                subscription.add(subscriber);
-                return subscriber;
-            }
-        };
-    }
-
-    @Override
-    public Observable<News> getNewsList(String date) {
-        return WebServiceProvider.get().getNewsList(date).lift(this.<News>bind());
-    }
-
-    @Override
-    public Observable<TopNews> getTopNewsList() {
-        return WebServiceProvider.get().getTopNewsList().lift(this.<TopNews>bind());
     }
 
 }
